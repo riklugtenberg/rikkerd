cdcd5d99fd8f4d883cb514a30af63db3228a65bc
riklugtenberg <riklugtenberg@hotmail.com>
2017-03-03 18:30:00 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int uitkomst = Rekensom.minSom(9, 2);
+
+            Rekensom som = new Rekensom();
+            int uitkomst2 = som.plusSom(9, 9);
+
+            Console.WriteLine("uitkomst 1 = " + uitkomst);
+            Console.WriteLine("uitkomst 2 = " + uitkomst2);
+
+        }
+    }
+}
---
+++ Rekensom.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Rekensom
+    {
+        public static int minSom(int getal1, int getal2)
+        {
+            return getal1 - getal2;
+        }
+        public int plusSom(int getal1, int getal2)
+        {
+            return getal2 + getal1;
+        }
+    }
+}
